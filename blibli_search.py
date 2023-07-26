181aa54ebe96915e8ddbc55d19d71fdea4160848
samycrash <samuelcrash7@gmail.com>
2023-07-26 20:22:57 +0700

    updating pagination dan penambahan opsi show item

--- blibli_search.py
+++ blibli_search.py
@@ -1,11 +1,13 @@
 import requests
 import json
+import time
 
 
-def calling_page(pages=1, keywords='', offset=0): 
+def calling_page(pages=1, keywords='', offset=0, item_need=0): 
     load_page = True
     page = pages
-    start_point = 0
+    start_point = offset
+    product_bin =[]
     while load_page:
         search_term = '%20'.join(keywords.split(' '))
         domain = 'https://www.blibli.com'
@@ -72,31 +74,59 @@ def calling_page(pages=1, keywords='', offset=0):
             }
 
             # print(product)
+            product_bin.append(product)
             yield product
+            if item_need != 0:                
+                if len(product_bin) >= item_need:
+                    load_page= False
+                    break                                    
         
-        current_page = int(data['data']['paging']['page'])
-        total_page = int(data['data']['paging']['total_page'])
-        print(f"you're in page {current_page} from {total_page}")
-        if current_page < total_page:
-            next_page = input('tampilkan lebih banyak? (y/n) :')
-            if next_page == 'y':
-                load_page = True
-                # print(page)
-                page = page +1
-                start_point = start_point+40
-            else:
+        if item_need==0:
+            current_page = int(data['data']['paging']['page'])
+            total_page = int(data['data']['paging']['total_page'])
+            print(f"you're in page {current_page} from {total_page}")        
+            if current_page < total_page:
+                next_page = input('tampilkan lebih banyak? (y/n) :')
+                if next_page == 'y':                                  
+                    page = page +1
+                    start_point = start_point+40
+                else:
+                    load_page= False
+            elif current_page >= total_page:
+                load_page = False
+        else:    
+            current_page = int(data['data']['paging']['page'])
+            total_page = int(data['data']['paging']['total_page'])                    
+            if current_page >= total_page:
                 load_page= False
+            page = page +1
+            start_point = start_point+40            
+        time.sleep(5)
 
 
 run = True
 while run:
-    search_item = input('masukkan barang yang dicari: ')    
-    App_bot = calling_page(keywords=search_item)
-    item_index =1
+    search_item = input('masukkan barang yang dicari: ')
+    while True:
+        try:
+            opsi_show_item = int(input('opsi tampilan [(1)tampilkan per halaman (2)sesuai banyak item yang dibutuhkan] (1)/(2)?: '))
+            break
+        except ValueError:
+            print('masukkan opsi "1" atau opsi "2" ')
+    if opsi_show_item==2:
+        while True:
+            try:
+                need_item = int(input('masukkan banyak item: '))
+                App_bot = calling_page(keywords=search_item, item_need= need_item)
+                break
+            except ValueError:
+                print('masukkan hanya data angka!!!')
+    if opsi_show_item== 1:
+        App_bot = calling_page(keywords=search_item)
+    items =[]
     for data in App_bot:
-        print(f'({item_index}) {data}')
-        item_index = item_index+1
-
+        items.append(data)
+        print(f'({len(items)}) {data}')        
 
     running_back = input('cari barang yang lain? (y/n): ')
     if running_back== 'y':
