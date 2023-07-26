466b56328a54d80709df23d12cedcf442ba5631b
samycrash <samuelcrash7@gmail.com>
2023-07-26 11:07:33 +0700

    tampilkan nama produk

--- blibli_search.py
+++ blibli_search.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 url = 'https://www.blibli.com/backend/search/products?sort=&page=2&start=40&searchTerm=acer%20predator&intent=true&merchantSearch=true&multiCategory=true&customUrl=&&channelId=web&showFacet=false&userIdentifier=657116261.U.5578713475148559.1690342388&isMobileBCA=false'
 
@@ -7,5 +8,11 @@ headers= {
 }
 
 r = requests.session()
-response = requests.get(url=url, headers= headers)
-print(response)
+response = r.get(url=url, headers= headers)
+data = json.loads(response.text)
+products = data['data']['products']
+
+for item in range(len(products)):
+    product_name  = products[item]['name']
+
+    print(f'({item+1}) {product_name}')
